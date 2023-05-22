0f9932370567c6684b6140a0dace7dccc255d80a
Levi-B4 <levi.butler4@gmail.com>
2023-05-22 13:19:17 -0400

    Outline of program flow

---
+++ Main.py
@@ -0,0 +1,10 @@
+#make program accept paramerter of directory to organize
+
+#if no path given
+#What is the path of the directory you would like to organize?
+
+#How do you want to organize it?
+#[fileName, fileType, fileSize, fileCreationDate]
+
+#open directory
+#is this how it should be organized? if not restart program at point of how do you want to organize it, if yes then exit program
