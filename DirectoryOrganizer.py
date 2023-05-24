e9b9391ae95f4b38ac45d3b00222f1eb5cd152a0
Levi-B4 <levi.butler4@gmail.com>
2023-05-23 23:53:16 -0400

    Implimenting command line arguments and verifying input

--- DirectoryOrganizer.py
+++ DirectoryOrganizer.py
@@ -1,9 +1,9 @@
 import os
-
-#make program accept paramerter of directory to organize
+import sys
+#make program accept parameter of directory to organize
 
 #if no path given
-#What is the path of the directory you would like to organize?
+    #What is the path of the directory you would like to organize?
 
 #How do you want to organize it?
 #[fileName, fileType, fileSize, fileCreationDate]
@@ -11,16 +11,40 @@ import os
 #open directory
 #is this how it should be organized? if not restart program at point of how do you want to organize it, if yes then exit program
 
-directoryInput = input("What is the path of the directory you would like to organize? (enter 'this' for current directory)")
+
+directoryInput = ""
+
+# Check for command line arguments
+args = sys.argv
+if len(args) > 1:
+    directoryInput = args[1]
+    print(f"Running Directory Organizer with argument {directoryInput}")
 
 
-if directoryInput == "this":
-    directory = os.getcwd()
-else:
-    directory = directoryInput #add error handling for invalid directory
+#verify directoryInput
+if directoryInput == None or "":
+    print("No directory given.")
+elif str.lower(directoryInput) == "this":
+    directoryInput = os.getcwd()
 
-directoryFileList = os.listdir(directory)
+while(os.path.isdir(directoryInput) == False):
+    print("Please enter a valid directory to organize")
+    directoryInput = input("What is the path of the directory you would like to organize? (enter 'this' for current directory or 'exit' to exit program)")
+    if str.lower(directoryInput) == "this":
+        directoryInput = os.getcwd()
+    elif str.lower(directoryInput) == "exit":
+        print("Exiting Directory Organizer...")
+        sys.exit()
 
-print(directoryFileList)
+
+#Stating directory to organize
+directoryName = os.path.dirname(directoryInput)
+print(f"Organizing {directoryName}")
+
+input("Press Enter to continue...")
+
+#List files in directory
+directoryFiles = os.listdir(directoryInput)
+print(directoryFiles)
 
 input("Press Enter to continue...")
