2469789bc37868c40d17170062a8298a5ea282ca
Levi-B4 <levi.butler4@gmail.com>
2023-05-24 12:11:32 -0400

    Updated README

--- DirectoryOrganizer.py
+++ DirectoryOrganizer.py
@@ -1,16 +1,20 @@
 import os
 import sys
-#make program accept parameter of directory to organize
 
-#if no path given
-    #What is the path of the directory you would like to organize?
-
-#How do you want to organize it?
-#[fileName, fileType, fileSize, fileCreationDate]
-
-#open directory
-#is this how it should be organized? if not restart program at point of how do you want to organize it, if yes then exit program
-
+###
+#                   outline
+#
+# make program accept parameter of directory to organize
+#
+# if no path given
+#   What is the path of the directory you would like to organize?
+#
+# How do you want to organize it?
+# [fileName, fileType, fileSize, fileCreationDate]
+#
+# open directory
+# is this how it should be organized? if not restart program at point of how do you want to organize it, if yes then exit program
+###
 
 directoryInput = ""
 
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# DirectoryOrganizer
+# DirectoryOrganizer (Still in production)
+
+## Description
+Organizes a directory in a user specified way. Can be ran directly or ran from the command line to be optionally be given a directory path as an argument.
+
+## Inspiration
+I have a friend wanting to learn python, so Im doing a project with him to help him learn. This is the project he chose.
+
+## How to Run
+- Install python 3.11 or later
+- Add python to environment variables
+- Run in terminal using `python path/to/DirectoryOrganizer.py' appending a directory path as an argument if wanted.
