4531ce20632e36c487647b3c63f442236a63b494
Levi-B4 <levi.butler4@gmail.com>
2023-05-26 23:21:58 -0400

    Restructured outline

--- DirectoryOrganizer.py
+++ DirectoryOrganizer.py
@@ -9,12 +9,20 @@ import sys
 # if no path given
 #   What is the path of the directory you would like to organize?
 #
-# How do you want to organize it?
-# [fileName, fileType, fileSize, fileCreationDate]
+# show extensions and how many of each file will be in each directory
 #
-# open directory
-# is this how it should be organized? if not restart program at point of how do you want to organize it, if yes then exit program
+# confirm organization
+#
+# complete organization
 ###
+
+# Functions TODO: break current code into functions
+def OrganizeDirByName(directoryPath):
+    directoryFileNames = os.listdir(directoryPath)
+    print(f"non sorted: {directoryFileNames}")
+    return sorted(directoryFileNames)
+
+
 
 directoryInput = ""
 
@@ -25,7 +33,7 @@ if len(args) > 1:
     print(f"Running Directory Organizer with argument {directoryInput}")
 
 
-#verify directoryInput
+# verify directoryInput
 if directoryInput == None or "":
     print("No directory given.")
 elif str.lower(directoryInput) == "this":
@@ -33,7 +41,8 @@ elif str.lower(directoryInput) == "this":
 
 while(os.path.isdir(directoryInput) == False):
     print("Please enter a valid directory to organize")
-    directoryInput = input("What is the path of the directory you would like to organize? (enter 'this' for current directory or 'exit' to exit program)")
+    directoryInput = input("What is the path of the directory you would like to organize?\n" +
+                           "Please enter 'this' for current directory or 'exit' to exit program\n")
     if str.lower(directoryInput) == "this":
         directoryInput = os.getcwd()
     elif str.lower(directoryInput) == "exit":
@@ -41,14 +50,35 @@ while(os.path.isdir(directoryInput) == False):
         sys.exit()
 
 
-#Stating directory to organize
-directoryName = os.path.dirname(directoryInput)
+# Stating directory to organize
+directoryName = os.path.basename(directoryInput)
 print(f"Organizing {directoryName}")
 
+# confirm directory to organize
 input("Press Enter to continue...")
 
-#List files in directory
-directoryFiles = os.listdir(directoryInput)
-print(directoryFiles)
+
+# Preview organization
+# get each file extentions and how many of each
+print("There will be numberOfUniqueExtensions folders")
+# list unique extensions and their count
+
+
+
+# Confirm organization
+
+
+
+
+# Organize
+
 
 input("Press Enter to continue...")
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # DirectoryOrganizer (Still in production)
 
 ## Description
-Organizes a directory in a user specified way. Can be ran directly or ran from the command line to pass a directory path as an argument.
-
-## Inspiration
-I have a friend wanting to learn python, so Im doing a project with him to help him learn. This is the project he chose.
+Organizes a directory by grouping files into sub-directories.
 
 ## How to Run
 - Install python 3.11 or later
-- Add python to environment variables
-- Run in terminal using `python path/to/DirectoryOrganizer.py' appending a directory path as an argument if wanted.
+- Run in terminal using `python DirectoryOrganizer.py` appending a directory path as an argument if wanted.
+- TestDirectory is not part of the program, but is a viable testing ground for the organizer
