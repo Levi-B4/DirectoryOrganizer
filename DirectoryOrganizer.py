0fd1539e8fbcd1f9d3c211ec6f2a36e65c15daf0
Levi-B4 <levi.butler4@gmail.com>
2023-05-28 18:00:51 -0400

    Better organized into functions and completed program

--- DirectoryOrganizer.py
+++ DirectoryOrganizer.py
@@ -1,84 +1,92 @@
 import os
 import sys
 
-###
-#                   outline
-#
-# make program accept parameter of directory to organize
-#
-# if no path given
-#   What is the path of the directory you would like to organize?
-#
-# show extensions and how many of each file will be in each directory
-#
-# confirm organization
-#
-# complete organization
-###
-
-# Functions TODO: break current code into functions
-def OrganizeDirByName(directoryPath):
-    directoryFileNames = os.listdir(directoryPath)
-    print(f"non sorted: {directoryFileNames}")
-    return sorted(directoryFileNames)
 
 
+# Functions
+def readCommandLineArguments():
+    args = sys.argv
+    if len(args) > 1:
+        arg = args[1]
+        print(f"Running Directory Organizer with argument {arg}")
+        return arg
+    else:
+        return ""
 
-directoryInput = ""
+def verifyDirectoryInput(directory):
+    if(directory == None or ""):
+        print("No directory given.")
+        return 
+    elif str.lower(directory) == "this":
+        return True
+    elif str.lower(directory) == "exit":
+        print("Exiting Directory Organizer...")
+        sys.exit()
+    elif os.path.isdir(directory):
+        return True
+    else:
+        return False
 
-# Check for command line arguments
-args = sys.argv
-if len(args) > 1:
-    directoryInput = args[1]
-    print(f"Running Directory Organizer with argument {directoryInput}")
+def OrganizeDirectoryIntoDictionary(directoryPath, fileNamesList):
+    subDirectories = {}
+    for fileName in fileNamesList:
+        extensionSubDirectoryName = os.path.splitext(fileName)[1][1:]
+        if extensionSubDirectoryName in subDirectories:
+            subDirectories[extensionSubDirectoryName].append(fileName)
+        else:
+           subDirectories[extensionSubDirectoryName] = [fileName]
+    return subDirectories
+
+def displayOrganizationPreview(subDirectoriesDictionary):
+    print("Organization Preview:\n" +
+          f"There will be {len(subDirectoriesDictionary)} new subdirectories in this directory\n")
+    for key in subDirectoriesDictionary:
+        print(f"{key} will contain {len(subDirectoriesDictionary[key])} files")
+
+def moveFilesToSubdirectories(directoryPath, fileNamesList):
+    directoryAbsolutePath = os.path.abspath(directoryPath)
+    for subDirectory in subDirectoriesDictionary:
+        os.mkdir(subDirectory)   ## makes directory correctly
+        for fileName in subDirectoriesDictionary[subDirectory]:
+            oldFilePath = os.path.join(directoryAbsolutePath, fileName)  ## cannot find path
+            newFilePath = os.path.join(directoryAbsolutePath, subDirectory, fileName)
+            os.replace(oldFilePath, newFilePath)
+            #TestDirectory
 
 
-# verify directoryInput
-if directoryInput == None or "":
-    print("No directory given.")
-elif str.lower(directoryInput) == "this":
-    directoryInput = os.getcwd()
 
-while(os.path.isdir(directoryInput) == False):
+# main
+directoryInput = readCommandLineArguments()
+
+
+    ## Verify directory input
+while(not verifyDirectoryInput(directoryInput)):
     print("Please enter a valid directory to organize")
     directoryInput = input("What is the path of the directory you would like to organize?\n" +
                            "Please enter 'this' for current directory or 'exit' to exit program\n")
-    if str.lower(directoryInput) == "this":
-        directoryInput = os.getcwd()
-    elif str.lower(directoryInput) == "exit":
-        print("Exiting Directory Organizer...")
-        sys.exit()
+    
+if str.lower(directoryInput) != "this":
+    os.chdir(directoryInput)
 
+directory = os.getcwd()
 
-# Stating directory to organize
-directoryName = os.path.basename(directoryInput)
+    ## Get directory name and list of files in directory
+directoryName = os.path.basename(directory)
+fileNames = os.listdir(directory)
+
 print(f"Organizing {directoryName}")
-
-# confirm directory to organize
-input("Press Enter to continue...")
-
-
-# Preview organization
-# get each file extentions and how many of each
-print("There will be numberOfUniqueExtensions folders")
-# list unique extensions and their count
+subDirectoriesDictionary = OrganizeDirectoryIntoDictionary(directory, fileNames)
+displayOrganizationPreview(subDirectoriesDictionary)
 
 
+    ## finalize organization
+confirm = input("Confirm organization? (y/n)\n")
+while str.lower(confirm) != "y" and str.lower(confirm) != "n":
+    confirm = input("Please enter 'y' to confirm organization or 'n' to cancel the organization.\n")
 
-# Confirm organization
-
-
-
-
-# Organize
-
-
-input("Press Enter to continue...")
-
-
-
-
-
-
-
-
+if str.lower(confirm) == "y":
+    moveFilesToSubdirectories(directory, fileNames)
+    print(f"{directoryName} has been organized\nExiting Directory Organizer...")
+else:
+    print("Organization cancelled\nExiting Directory Organizer...")
+    sys.exit()
